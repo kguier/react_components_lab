a88bf44396fd0330cdeea7b3ec11826198a6405d
Kira <knguier12@gmail.com>
2023-12-03 12:37:39 -0500

    Completed Array and finished Lab

--- src/components/NamesList/NamesList.jsx
+++ src/components/NamesList/NamesList.jsx
@@ -1,9 +1,12 @@
 const NamesList = ({}) => {
+  const names = [
+    { firstName: "Carol", lastName: "Park" },
+    { firstName: "Katie", lastName: "Pierce" },
+    { firstName: "Garrett", lastName: "Guier" },
+    { firstName: "Daphne", lastName: "Guier-Park" },
+  ];
   return (
     <header>
-      <h1>{displayedName}</h1>
-      <h1>{displayedName}</h1>
-      <h1>{displayedName}</h1>
       <h1>{displayedName}</h1>
     </header>
   );
